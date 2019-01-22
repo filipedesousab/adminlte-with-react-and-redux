241e755fc7ec2f0272fc8ca2a4ae1e29025847d8
Filipe de Sousa Botelho <filipegame07@gmail.com>
2019-01-22 00:26:18 -0300

    Atualizado página Home

--- src/Home/HomeComponent.jsx
+++ src/Home/HomeComponent.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import { Content, Box } from '../common/ui-layout';
+import { Label } from '../common/ui-elements';
 
 /** @type {function} Página inicial da aplicação */
 const HomeComponent = () => (
-  <Content title="Home" subtitle="Subtitulo" breadcrumb={[{ label: 'Home', href: '#' }]}>
-    <Box title="Teste" color="danger">
-    Página inicial
+  <Content
+    title={<Label icon="fa fa-home">Home</Label>}
+    subtitle={<Label>Subititulo</Label>}
+    breadcrumb={[{ label: 'Home', href: '#' }]}
+  >
+    <Box title={<Label icon="fa fa-cog">Teste</Label>} color="danger">
+      Página Home - Atualmente usando para teste
     </Box>
   </Content>
 );
