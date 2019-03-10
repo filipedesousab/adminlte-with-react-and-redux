02deadcab99e3e091cbc6e9dc9269a1f68f0bb84
Filipe de Sousa Botelho <filipegame07@gmail.com>
2019-03-10 03:37:58 -0300

    test([Tooltip, UIE030]): remover stories desnecessários
    O story title não se aplica ao Tootip
    Os stories relacionados a delay deixaram de funcionar com a ultima
    alteração no componente.

--- src/common/ui-elements/general/Tooltip/tooltip.stories.jsx
+++ src/common/ui-elements/general/Tooltip/tooltip.stories.jsx
@@ -14,28 +14,5 @@ storiesOf('ui-elements/general/Tooltip [Tooltip, UIE030]', module)
   ), {
     notes: `Texto a ser exibido no Tooltip. Deve ser string.
             O children é o elemento a receber o Tooltip.`,
-  })
-
-  .add('title', () => (
-    <Tooltip description="Descrição do Tooltip" title="Um título">
-      <Button><Label>Botão</Label></Button>
-    </Tooltip>
-  ), {
-    notes: 'Texto a ser exibido no título do Tooltip. Deve ser string.',
-  })
-
-  .add('delay true', () => (
-    <Tooltip description="Tooltip exibido em 1500ms">
-      <Button><Label>Espere 1.5s</Label></Button>
-    </Tooltip>
-  ), {
-    notes: 'O delay aplica um atraso em milissegundos para exibir o Tooltip, quando true aplica 1500ms.',
-  })
-
-  .add('delay 2000ms', () => (
-    <Tooltip delay={2000} description="Tooltip exibido em 2000ms">
-      <Button><Label>Espere 2s</Label></Button>
-    </Tooltip>
-  ), {
-    notes: 'O delay aplica um atraso em milissegundos para exibir o Tooltip, quando true aplica 600ms.',
   });
+
